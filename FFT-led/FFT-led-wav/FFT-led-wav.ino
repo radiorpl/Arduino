b4596db7191ad1590544750c626eea267cf395e5
Richard Levinson <radiorpl@users.noreply.github.com>
2017-12-23 15:25:30 -0500

    better settings

--- FFT-led/FFT-led-wav/FFT-led-wav.ino
+++ FFT-led/FFT-led-wav/FFT-led-wav.ino
@@ -30,11 +30,11 @@ AudioControlSGTL5000     sgtl5000_1;     //xy=670,337
 
 //fft range arrays
 //set for 87 wide
-int lowBound [3] = {2, 6, 15};					//lower frequency bounds for eq channels
-int highBound[3] = {5, 14, 150};				//upper frequency bounds for eq channels
+int lowBound [3] = {2, 6, 13};					//lower frequency bounds for eq channels
+int highBound[3] = {4, 11, 120};				//upper frequency bounds for eq channels
 //int ledPin   [9] = {0, 1, 2, 3, 4, 5, 35, 36, 37};			//led pins, grouped in order, harder to hook up
 int ledPin   [9] = {0, 3, 35, 1, 4, 36, 2, 5, 37};			//led pins, grouped for 3 pins per eq channel
-int ledBright [10] = {0, 10, 20, 30, 50, 70, 90, 110, 170, 255};		//led brightness settings
+int ledBright [6] = {0, 20, 50, 90, 170, 255};		//led brightness settings
 int l; //number of leds for setup loop
 
 void setup() {
@@ -86,142 +86,83 @@ void loop() {
 			  k = j+3;
 		      n = fft1024_1.read(lowBound[i], highBound[i]);
 		      if (n >= 0.02) {
-				 if (0.02 < n <= 0.03){
+				 if (0.02 < n <= 0.05){
 					  b = 1;
 					  c = 0;
 					  d = 0;
 				  }
-				  else if (0.03 < n <= 0.04){
+				  else if (0.05 < n <= 0.08){
 					  b = 3;
 					  c = 0;
 					  d = 0;
 				  }
-				  else if (0.04 < n <= 0.05){
+				  else if (0.08 < n <= 0.11){
 					  b = 4;
 					  c = 0;
 					  d = 0;
 				  }
-				  else if (0.05 < n <= 0.06){
+				  else if (0.11 < n <= 0.15){
 					  b = 5;
 					  c = 0;
 					  d = 0;
 				  }
-				  else if (0.06 < n <= 0.07){
-					  b = 6;
-					  c = 0;
-					  d = 0;
-				  }
-				  else if (0.07 < n <= 0.08){
-					  b = 7;
-					  c = 0;
-					  d = 0;
-				  }
-				  else if (0.08 < n <= 0.09){
-					  b = 8;
-					  c = 0;
-					  d = 0;
-				  }
-				  else if (0.09 < n <= 0.1){
-					  b = 9;
-					  c = 0;
-					  d = 0;
-				  }
-				  else if (0.1 < n <= 0.11){
-					  b = 9;
+				  else if (0.15 < n <= 0.19){
+					  b = 5;
 					  c = 1;
 					  d = 0;
 				  }
-				  else if (0.11 < n <= 0.12){
-					  b = 9;
+				  else if (0.19 < n <= 0.23){
+					  b = 5;
 					  c = 2;
 					  d = 0;
 				  }
-				  else if (0.12 < n <= 0.13){
-					  b = 9;
+				  else if (0.23 < n <= 0.28){
+					  b = 5;
 					  c = 3;
 					  d = 0;
 				  }
-				  else if (0.13 < n <= 0.14){
-					  b = 9;
+				  else if (0.28 < n <= 0.33){
+					  b = 5;
 					  c = 4;
 					  d = 0;
 				  }
-				  else if (0.14 < n <= 0.15){
-					  b = 9;
+				  else if (0.33 < n <= 0.39){
+					  b = 5;
 					  c = 5;
 					  d = 0;
 				  }
-				  else if (0.15 < n <= 0.16){
-					  b = 9;
-					  c = 6;
-					  d = 0;
-				  }
-				  else if (0.16 < n <= 0.17){
-					  b = 9;
-					  c = 7;
-					  d = 0;
-				  }
-				  else if (0.17 < n <= 0.18){
-					  b = 9;
-					  c = 8;
-					  d = 0;
-				  }
-				  else if (0.18 < n <= 0.20){
-					  b = 9;
-					  c = 9;
-					  d = 0;
-				  }
-				  else if (0.20 < n <= 0.22){
-					  b = 9;
-					  c = 9;
+				  else if (0.39 < n <= 0.45){
+					  b = 5;
+					  c = 5;
 					  d = 1;
 				  }
-				  else if (0.22 < n <= 0.24){
-					  b = 9;
-					  c = 9;
+				  else if (0.45 < n <= 0.52){
+					  b = 5;
+					  c = 5;
 					  d = 2;
 				  }
-				  else if (0.24 < n <= 0.26){
-					  b = 9;
-					  c = 9;
+				  else if (0.52 < n <= 0.59){
+					  b = 5;
+					  c = 5;
 					  d = 3;
 				  }
-				  else if (0.26 < n <= 0.30){
-					  b = 9;
-					  c = 9;
+				  else if (0.59 < n <= 0.67){
+					  b = 5;
+					  c = 5;
 					  d = 4;
 				  }
-				  else if (0.30 < n <= 0.34){
-					  b = 9;
-					  c = 9;
+
+				  else if (n > 0.67){
+					  b = 5;
+					  c = 5;
 					  d = 5;
-				  }
-				  else if (0.34 < n <= 0.38){
-					  b = 9;
-					  c = 9;
-					  d = 6;
-				  }
-				  else if (0.38 < n <= 0.44){
-					  b = 9;
-					  c = 9;
-					  d = 7;
-				  }
-				  else if (0.44 < n <= 0.50){
-					  b = 9;
-					  c = 9;
-					  d = 8;
-				  }
-				  else if (n > 0.50){
-					  b = 9;
-					  c = 9;
-					  d = 9;
 				  }
 				  	digitalWriteFast(ledPin[i], ledBright[b]);
 					digitalWriteFast(ledPin[j], ledBright[c]);
 					digitalWriteFast(ledPin[k], ledBright[d]);
 					Serial.print(n);
 		        	Serial.print(" ");
-					delay(2);
+					delay(50);
 		      } 
 			  else {
 				  	digitalWriteFast(ledPin[i], ledBright[0]);
